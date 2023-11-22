645242f6a481b6ad1f9648061681416775252d62
Thomas Clarkson <tcla272@UoA.auckland.ac.nz>
2023-11-22 14:45:06 +1300

    Refactored test case and added config file Refactored code such that it works with arbitrary Natom. Added config file with parameters, including a path to save output images.

--- NesiConsultancy/AtomsCavityBasisDemo.py
+++ NesiConsultancy/AtomsCavityBasisDemo.py
@@ -5,79 +5,64 @@ Created on Wed Nov  8 09:41:28 2023
 @author: aell060
 """
 
+#%% Imports
 import qutip as qt
 import numpy as np
+import matplotlib.pyplot as plt
 import time
+import configparser
 
-start_time=time.time()
-natoms = 1
+#%% Read in configs
+configs = configparser.ConfigParser()
+configs.read('config.ini')
 
-eps = 1
-g = 1
-kappa = 1/2
-gamma = 1
+# Assign parameters
+eps = configs['PARAMS'].getfloat('eps')
+g = configs['PARAMS'].getfloat('g')
+kappa = configs['PARAMS'].getfloat('kappa')
+gamma = configs['PARAMS'].getfloat('gamma')
 
-Ncav = 15
-Nat = 2
+# Get basis size
+Ncav = configs['PARAMS'].getint('Ncav')
+Natom = configs['PARAMS'].getint('Natom')
 
-dt = 1/100
-tmax = 100
+# Build solve times
+dt = configs['TIMES'].getfloat('dt')
+tmax = configs['TIMES'].getfloat('tmax')
 tlist = np.arange(0,tmax+dt,dt)
 
-idcav = qt.qeye(Ncav); idatom = qt.qeye(Nat)
+#%% Build Hamiltonian
+start_time=time.time()
 
-if (natoms ==1):
-    a = qt.tensor(qt.destroy(Ncav),idatom)
-    sm = qt.tensor(idcav,qt.sigmam())
+atomI = qt.tensor([qt.qeye(2) for i in range(Natom)])
+cavI = qt.qeye(Ncav)
+
+sm = [qt.tensor([qt.sigmam() if j==i else qt.qeye(2) for j in range(Natom)]) for i in range(Natom)]
+sm = [qt.tensor(cavI,i) for i in sm]
+
+a = qt.tensor(qt.destroy(Ncav),atomI)
+c_ops = [np.sqrt(2*kappa)*a]
+
+H = eps*(a + a.dag())
+for i in range(Natom):
+    H = H + g*(a.dag()*sm[i] + a*sm[i].dag())
+    c_ops.append(np.sqrt(gamma)*sm[i])
     
-    H = g * (a.dag() * sm + sm.dag() * a) + eps * (a.dag() + a )
-    c_ops = [np.sqrt(2*kappa)*a,np.sqrt(gamma) * sm]
-    
-    rhoss = qt.steadystate(H,c_ops)
-    print("Cavity mode commutator = {}".format(np.round(qt.expect(a*a.dag()-a.dag()*a,rhoss),5)))
-    rho0 = qt.mesolve(H,rhoss,tlist,c_ops,progress_bar=True)
-    print('done')
-    qt.hinton(rho0.states[-1])
-    qt.hinton(rho0.states[-1].ptrace(0))
-    qt.hinton(rho0.states[-1].ptrace(1))
-elif (natoms ==2):
-    a = qt.tensor(qt.destroy(Ncav),idatom,idatom)
-    sm = qt.tensor(idcav,qt.sigmam(),idatom)
-    sm2 = qt.tensor(idcav,idatom,qt.sigmam())
-    
-    H = g * (a.dag() * sm + sm.dag() * a) + g * (a.dag() * sm2 + sm2.dag() * a)+ eps * (a.dag() + a )
-    c_ops = [np.sqrt(2*kappa)*a,np.sqrt(gamma) * sm,np.sqrt(gamma) * sm2]
-    
-    rhoss = qt.steadystate(H,c_ops)
-    print("Cavity mode commutator = {}".format(np.round(qt.expect(a*a.dag()-a.dag()*a,rhoss),5)))
-    rho0 = qt.mesolve(H,rhoss,tlist,c_ops,progress_bar=True)
-    print('done')
-    qt.hinton(rho0.states[-1])
-    qt.hinton(rho0.states[-1].ptrace(0))
-    qt.hinton(rho0.states[-1].ptrace(1))
+#%% Solve master equation
+rhoss = qt.steadystate(H,c_ops)
+print("Cavity mode commutator = {}".format(np.round(qt.expect(a*a.dag()-a.dag()*a,rhoss),5)))
+rho0 = qt.mesolve(H,rhoss,tlist,c_ops,progress_bar=True)
+solve_time = np.round(time.time()-start_time,2)
+print("Time taken till solver = {}s.".format(solve_time))
+
+fig0,ax0 = qt.hinton(rho0.states[-1])
+fig1,ax1 = qt.hinton(rho0.states[-1].ptrace(0))
+qt.hinton(rho0.states[-1].ptrace(1))
+if Natom >=2:
     qt.hinton(rho0.states[-1].ptrace(2))
-elif (natoms ==4):
-    a = qt.tensor(qt.destroy(Ncav),idatom,idatom,idatom,idatom)
-    sm = qt.tensor(idcav,qt.sigmam(),idatom,idatom,idatom)
-    sm2 = qt.tensor(idcav,idatom,qt.sigmam(),idatom,idatom)
-    sm3 = qt.tensor(idcav,idatom,idatom,qt.sigmam(),idatom)
-    sm4 = qt.tensor(idcav,idatom,idatom,idatom,qt.sigmam())
-    
-    H = g * (a.dag() * sm + sm.dag() * a) + g * (a.dag() * sm2 + sm2.dag() * a)\
-        +g * (a.dag() * sm3 + sm3.dag() * a) + g * (a.dag() * sm4 + sm4.dag() * a)\
-        + eps * (a.dag() + a )
-    c_ops = [np.sqrt(2*kappa)*a,np.sqrt(gamma) * sm,np.sqrt(gamma) * sm2\
-             ,np.sqrt(gamma) * sm3,np.sqrt(gamma) * sm4]
-    
-    rhoss = qt.steadystate(H,c_ops)
-    print("Cavity mode commutator = {}".format(np.round(qt.expect(a*a.dag()-a.dag()*a,rhoss),5)))
-    rho0 = qt.mesolve(H,rhoss,tlist,c_ops,progress_bar=True)
-    print('done')
-    qt.hinton(rho0.states[-1])
-    qt.hinton(rho0.states[-1].ptrace(0))
-    qt.hinton(rho0.states[-1].ptrace(1))
-    qt.hinton(rho0.states[-1].ptrace(2))
-else:
-    print("You chose and invalid number of atoms (1,2, or 4 allowed).")
+
+#%% Save figure
+plt.figure(fig1)
+plt.savefig(configs['OUTPUTS']['savefig'],format='png',bbox_inches='tight')
 total_time = np.round(time.time()-start_time,2)
-print("Time taken = {}s.".format(total_time))
+print("Total time taken = {}s.".format(total_time))
